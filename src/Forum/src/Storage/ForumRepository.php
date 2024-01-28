dd2f7757f69fbf557a153382dfe0b5fd17c531d6
Joey Smith <jsmith@webinertia.net>
2024-01-27 20:52:16 -0600

    Adding Language directories for all modules Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Forum/src/Storage/ForumRepository.php
+++ src/Forum/src/Storage/ForumRepository.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace Forum\Storage;
 
 use Axleus\Storage\AbstractRepository;
+use Axleus\Storage\RepositoryTrait;
 
 final class ForumRepository extends AbstractRepository
 {
+    use RepositoryTrait;
 
+    public function fetchIndex()
+    {
+
+    }
 }
